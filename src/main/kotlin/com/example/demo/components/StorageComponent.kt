6881b92dbbb9fd8065b09837aae3996a2a2ba25b
João Bini <joao.felipe@contabilizei.com.br>
2022-03-24 08:34:36 -0300

    Limitando tamanho do arquivo e content type.

--- src/main/kotlin/com/example/demo/components/StorageComponent.kt
+++ src/main/kotlin/com/example/demo/components/StorageComponent.kt
@@ -3,6 +3,7 @@ package com.example.demo.components
 import com.google.cloud.storage.BlobId
 import com.google.cloud.storage.BlobInfo
 import com.google.cloud.storage.Storage
+import org.apache.http.entity.ContentType
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
 import java.util.concurrent.TimeUnit
@@ -12,12 +13,25 @@ class StorageComponent(private val storage: Storage) {
 
     private val bucketName = "tdc-bucket"
 
+    private val allowedContentType =
+        listOf(
+            ContentType.IMAGE_JPEG.mimeType,
+            ContentType.IMAGE_PNG.mimeType,
+            ContentType.IMAGE_SVG.mimeType
+        )
+
     fun upload(file: MultipartFile): String? {
+
+        val contentType = file.contentType
+        val allowedType = allowedContentType.contains(contentType)
+
+        if (allowedType.not()) error("Content type not allowed!")
+
         return storage.create(buildBlobInfo(file), file.bytes).blobId.name
     }
 
     fun getDownloadUrl(filename: String): String {
-        return storage.get("tdc-bucket", filename )
+        return storage.get("tdc-bucket", filename)
             .signUrl(60, TimeUnit.HOURS, Storage.SignUrlOption.withV4Signature())
             .toString()
     }
